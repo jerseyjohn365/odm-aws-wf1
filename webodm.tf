d4f79b5a96842eecdd28d5df249e3625eca4e118
jerseyjohn365 <jjrussell9@gmail.com>
2026-04-24 17:09:29 -0500

    Restrict CIDR block for WebODM to specific IP

--- webodm.tf
+++ webodm.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "odm" {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["47.186.10.128"]
   }
   ingress {
     description = "ClusterODM"
